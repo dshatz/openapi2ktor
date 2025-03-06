2ae332a63134421b6f95bbc6ebcace7d2ca9c396
Daniels Šatcs <dev@dshatz.com>
2025-03-06 19:10:25 +0200

    Additional small test

--- processor/src/test/kotlin/com/dshatz/openapi2ktor/generators/KotlinxCodeGeneratorTest.kt
+++ processor/src/test/kotlin/com/dshatz/openapi2ktor/generators/KotlinxCodeGeneratorTest.kt
@@ -3,11 +3,16 @@ package com.dshatz.openapi2ktor.generators
 import com.dshatz.openapi2ktor.generators.Type.Companion.simpleType
 import com.dshatz.openapi2ktor.generators.models.KotlinxCodeGenerator
 import com.dshatz.openapi2ktor.utils.Packages
+import com.dshatz.openapi2ktor.utils.capitalize
+import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.CodeBlock
+import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
+import com.squareup.kotlinpoet.TypeSpec
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class KotlinxCodeGeneratorTest {
 
@@ -15,6 +20,7 @@ class KotlinxCodeGeneratorTest {
     @BeforeTest
     fun init() {
         generator = KotlinxCodeGenerator(TypeStore(), Packages("com.example"))
+        generator.responseMappings = KotlinxCodeGenerator.ResponseInterfaceResult(emptyList(), emptyMap())
     }
 
     @Test
@@ -37,6 +43,31 @@ class KotlinxCodeGeneratorTest {
                 Double::class.type().defaultValue(1.0)
             )
         }
+    }
+
+    @Test
+    fun `generate objects`() {
+        val user = generator.typeStore.addObject("User", "name" to String::class.type(), "age" to Int::class.type())
+        val fileSpecs = generator.generateObjects()
+        assertEquals(1, fileSpecs.size)
+        assertTrue(fileSpecs.first().hasType(user))
+    }
+
+
+    private fun TypeStore.addObject(name: String, vararg props: Pair<String, Type>): ClassName {
+        val cl = makeObjectClassName(name)
+        registerType("#/components/schema/$name",
+            Type.WithTypeName.Object(cl, props.toMap(), props.map { it.first }, emptyMap())
+        )
+        return cl
+    }
+
+    private fun makeObjectClassName(name: String): ClassName {
+        return ClassName("com.example.models", "components.schemas.${name.capitalize()}")
+    }
+
+    private fun FileSpec.hasType(cl: ClassName): Boolean {
+        return this.packageName == cl.packageName && this.members.find { it is TypeSpec && it.name == cl.simpleName } != null
     }
 
 }
