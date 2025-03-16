967366733bf90a77625451943ad2d1af1dc04966
Daniels Šatcs <dev@dshatz.com>
2025-03-16 05:37:37 +0200

    Publish

--- gradle-plugin/plugin/build.gradle.kts
+++ gradle-plugin/plugin/build.gradle.kts
@@ -16,7 +16,7 @@ gradlePlugin {
 }
 
 group = "com.dshatz.openapi2ktor"
-version = "1.0.0-SNAPSHOT1"
+version = project.findProperty("version") as String? ?: "0.1.0-SNAPSHOT1"
 
 kotlin {
     jvmToolchain(17)
@@ -36,6 +36,17 @@ publishing {
     }
     repositories {
         mavenLocal()
+        maven {
+            name = "MavenCentral"
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+
+            credentials {
+                username = System.getenv("mavenUsername") ?: ""
+                password = System.getenv("mavenPassword") ?: ""
+            }
+        }
     }
 }
 
@@ -45,46 +56,3 @@ dependencies {
     implementation(project(":processor"))
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.10")
 }
-
-/*mavenPublishing {
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-
-//    signAllPublications()
-
-    coordinates(group.toString(), "openapi2ktor", version.toString())
-
-    configure(GradlePlugin(JavadocJar.None()))
-
-    pom {
-        name = "OpenApi3 to Ktor"
-        description = "Ktor client and kotlin model generator."
-        inceptionYear = "2025"
-        url = "https://github.com/dshatz/openapi2ktor/"
-        licenses {
-            license {
-                name = "GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007"
-                url = "https://github.com/dshatz/openapi2ktor/blob/main/LICENSE"
-            }
-        }
-        developers {
-            developer {
-                id = "dshatz"
-                name = "Daniels Šatcs"
-                email = "dev@dshatz.com"
-            }
-        }
-        scm {
-            url = "https://github.com/dshatz/openapi2ktor"
-            connection = "git@github.com:dshatz/openapi2ktor.git"
-        }
-        withXml {
-            val dependenciesNode = asNode().get("dependencies") as? groovy.util.Node
-            dependenciesNode?.children()?.removeIf { dep ->
-                dep is groovy.util.Node &&
-                        dep.children().find { it is groovy.util.Node && it.name() == "artifactId" }?.let {
-                            (it as groovy.util.Node).text() == "processor"
-                        } ?: false
-            }
-        }
-    }
-}*/
